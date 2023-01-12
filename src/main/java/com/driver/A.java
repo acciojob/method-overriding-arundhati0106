9d971f9aaa1b09f1988ae995096ef25cd7b25dbf
Arundhati Mohanty <arundhati.mohanty1@gmail.com>
2023-01-13 00:15:02 +0530

    version 2... call parent method from child class

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -1,7 +1,11 @@
 package com.driver;
 
 public class A {
-    String meth() {
+//    public A() {
+//        A object = new A();
+//    }
+
+    public String meth() {
         return "Invoking method from class A";
     }
 }
--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,8 +1,13 @@
 package com.driver;
 
 public class B extends A {
+//    public B() {
+//        B object1 = new B();
+//    }
+
     @Override
-    String meth() {
+    public String meth() {
+        super.meth();
         return "Method is overridden in Extendend class B";
     }
 }
